f26cf257023ead43c644c1e7bf95e306df1bfdf2
Ai Makabi <a.macbee@gmail.com>
2013-11-28 04:29:44 +0900

    初期attributeを設定

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default["mecab"]["version"] = "0.996"
+default["mecab"]["checksum"] = "15baca0983a61c1a49cffd4a919463a0a39ef127"
+default["mecab"]["configure_options"] = "--with-charset=utf8  --enable-utf8-only"
+
+default["ipadic"]["version"] = "2.7.0-20070801"
+default["ipadic"]["checksum"] = "0d9d021853ba4bb4adfa782ea450e55bfe1a229b"
+default["ipadic"]["configure_options"] = "--with-charset=utf8 --enable-utf8-only"
