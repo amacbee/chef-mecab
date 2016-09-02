9b0949126db57d76ea8e72126c6c5f33ec92074a
tkuchiki <kuchiki.taku@gmail.com>
2016-09-02 12:33:26 +0900

    checksum を sha256 にする

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default["mecab"]["version"] = "0.996"
 default["mecab"]["download_id"] = "0B4y35FiV1wh7cENtOXlicTFaRUE"
-default["mecab"]["checksum"] = "15baca0983a61c1a49cffd4a919463a0a39ef127"
+default["mecab"]["checksum"] = "e073325783135b72e666145c781bb48fada583d5224fb2490fb6c1403ba69c59"
 default["mecab"]["configure_options"] = "--with-charset=utf8  --enable-utf8-only"
 
 default["ipadic"]["version"] = "2.7.0-20070801"
 default["ipadic"]["download_id"] = "0B4y35FiV1wh7MWVlSDBCSXZMTXM"
-default["ipadic"]["checksum"] = "0d9d021853ba4bb4adfa782ea450e55bfe1a229b"
+default["ipadic"]["checksum"] = "b62f527d881c504576baed9c6ef6561554658b175ce6ae0096a60307e49e3523"
 default["ipadic"]["configure_options"] = "--with-charset=utf8 --enable-utf8-only"
